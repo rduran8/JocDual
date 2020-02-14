1a78ac088cf386b3ab233c9efd9a103ede634f15
gferrer8 <50497179+gferrer8@users.noreply.github.com>
2020-02-14 15:41:34 +0100

    Add files via upload

---
+++ PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class PlayerData
+{
+    public int life;
+
+    public int food;
+
+    public PlayerData (Player player)
+    {
+
+        life = player.getLife();
+        food = player.getFood();
+        
+    }
+   
+}
